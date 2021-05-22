bd9f462714f98d9637fbd091aafe9149c68a5802
krishna450 <55475977+krishna450@users.noreply.github.com>
2021-05-22 11:54:14 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,10 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi,
+I am an Auto-Forwarder bot.
+
+My 👉 <a href='https://github.com/Ns-AnoNymouS/Auto-Forward-Bot'>Source Code</a> 👈"             
 
       #About text
       ABOUT = """
