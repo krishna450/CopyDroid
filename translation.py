63acd036d358490a19079448659b199f17a0d041
krishna450 <55475977+krishna450@users.noreply.github.com>
2021-05-22 11:55:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,10 +7,10 @@ I am an Auto-Forwarder bot.
 My 👉 <a href='https://github.com/Ns-AnoNymouS/Auto-Forward-Bot'>Source Code</a> 👈"             
 
       #About text
-      ABOUT = """
-<b>📝 Language:</b> Python 3
-<b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+       #ABOUT = """
+ #<b>📝 Language:</b> Python 3
+ #<b>🧰 Framework:</b> Pyrogram
+ #<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
+ #<b>📢 Channel:</b> NS BOT UPDATES
+ #<b>👥 Group:</b> Ns BOT SUPPORT
+ #<b>🌐Source Code:</b> Press Me 🥰"""
